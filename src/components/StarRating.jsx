a14dd1021e6c0856cb2b0134e14b497c68f17a1d
generalentropy <entropie@tuta.io>
2023-12-28 17:43:57 +0100

    update

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -3,7 +3,7 @@ import { Rating } from "react-simple-star-rating";
 export default function StarRating({ value }) {
   return (
     <Rating
-      fillColor={"#ffa726"}
+      fillColor={"#FFDF00"}
       readonly={true}
       initialValue={value}
       size="23"
