4b4a5e24fcdc2192e24193acd429d0ba4467bc50
generalentropy <entropie@tuta.io>
2023-12-28 13:37:53 +0100

    fake REST API

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function Cards({ children }) {
+  useEffect(() => {
+    const fetchData = async function () {
+      const res = await fetch("http://localhost:8000/serveurs");
+
+      const data = await res.json();
+
+      console.log(data);
+    };
+
+    fetchData();
+  });
+
   return <div className="cards-container">{children}</div>;
 }
 
